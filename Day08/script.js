4c00ba8ec7b0caa22c20391911cf3dd9565c0982
NayandG07 <nayandg8@gmail.com>
2026-07-15 17:49:18 +0530

    Day 8 initial commit

---
+++ Day08/script.js
@@ -0,0 +1,10 @@
+console.log("Hi there!");
+
+let name = "John";
+console.log(name);
+
+let num = 20;
+console.log(num);
+
+console.log("Type of name: " + typeof name);
+console.log("Type of num: " + typeof num);
