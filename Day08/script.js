db96e75fe44bcd53a366ced2463e728ddca3fbbd
NayandG07 <nayandg8@gmail.com>
2026-07-15 18:00:51 +0530

    updated

--- Day08/script.js
+++ Day08/script.js
@@ -1,10 +1,21 @@
 console.log("Hi there!");
 
-let name = "John";
-console.log(name);
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName, lastName);
 
 let num = 20;
 console.log(num);
 
-console.log("Type of name: " + typeof name);
+console.log("Type of firstName: " + typeof firstName);
 console.log("Type of num: " + typeof num);
+
+let dec = 5.6;
+console.log("Type of dec: " + typeof dec);
+
+console.log("Sum of num and dec: " + (num + dec));
+
+let bool1 = true;
+let bool2 = false;
+console.log("Type of bool1 & bool2: " + typeof bool1 + " & " + typeof bool2);
+
