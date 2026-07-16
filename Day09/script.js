b3c6ed3ae54cf35d2f5b3a948580e9df605581e3
NayandG07 <nayandg8@gmail.com>
2026-07-16 19:14:18 +0530

    Updated

--- Day09/script.js
+++ Day09/script.js
@@ -7,7 +7,7 @@ let obj = {
     sub: ["FS", "AI", "DL", "ML"],
     hobies: {
         sports: ["Football", "Cricket"],
-        songs: ["Anyone", "Under the Milky Way"],
+        listening: "Sapphire",
         test1: {
             test2: {
                 test3: {
@@ -40,3 +40,38 @@ console.log(obj)
 
 // Adding a new key 
 obj["isMarried"] = false;
+
+// console.log(obj.hobbies.test1.test2.test3.test4.test5[100]); // undefined because 100 is not a key in the object 
+
+let num = 10;
+
+for (let i = 0; i < num; i++) {
+    console.log(i)
+}
+
+let st = ["John", "Doe", "Jane", "Smith", "Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+
+for (let i = 0; i < st.length; i++) {
+    // console.log(st[i])
+    // for (let j = 0; j < st[i].length; j++) {
+    //     console.log(st[i][j])
+    // }
+    // Acess the first character of each name in the st array
+    console.log(st[i][0])
+}
+console.log(typeof st);
+
+let firstStudent = st[0];
+
+for (let i = 0; i < firstStudent.length; i++) {
+    console.log(firstStudent[i])
+}
+let alph = "abcdefghijklmnopqrstuvwxyz";
+for (let i = 0; i < alph.length; i++) {
+    if (i % 2 == 0) {
+        console.log('Even index',i, alph[i])
+    } else {
+        console.log('Odd index',i, alph[i])
+    }
+}
+
