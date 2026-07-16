e2e44d58e07f9f4f05501a10d3437c57c0f6c8f9
NayandG07 <nayandg8@gmail.com>
2026-07-16 18:04:41 +0530

    End

--- Day08/script.js
+++ Day08/script.js
@@ -18,4 +18,19 @@ console.log("Sum of num and dec: " + (num + dec));
 let bool1 = true;
 let bool2 = false;
 console.log("Type of bool1 & bool2: " + typeof bool1 + " & " + typeof bool2);
+console.log("\n");
+
+// Comparision operators - equal == / ===, not equal != / !==, greater than >, less than <, greater than or equal >=, less than or equal <=
+
+let num1 = 3;
+let num2 = 4;
+console.log(num1 <= num2);
+
+if(num1 < num2) {
+    console.log("Won");
+} else if (num1 > num2) {
+    console.log("Lost");
+} else {
+    console.log("Draw");
+}
 
