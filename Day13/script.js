9288350e8b6e012c8c382b65ddaab52025286fc2
NayandG07 <nayandg8@gmail.com>
2026-07-22 18:22:11 +0530

    Updated

--- Day13/script.js
+++ Day13/script.js
@@ -1,16 +1,11 @@
-const clickFunc=()=>{
-    console.log("Button Clicked");
+const clickFunc = () => {
+    console.log("Submitted");
 }
-
-btn.addEventListener("click", clickFunc); // Adding event listener to button
-
-btn.addEventListener("click", ()=>{ // Using Anonymous function to add event listener to button
-    console.log("Button Clicked inside event listener");
-})
+btn.addEventListener("click", clickFunc);
 
 let form = document.getElementById("form");
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", (e) => {
     let fullName = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
@@ -20,11 +15,24 @@ form.addEventListener("submit", (e)=>{
         email,
         password
     }
-    e.defaultbehavior();
+    e.preventDefault();
 
     // localStorage.setItem("userData", user); // Storing user data in local storage
-    localStorage.setItem("userData", JSON.stringify(user)); // Storing user data in local storage as string
+    // localStorage.setItem("userData", JSON.stringify(user)); // Storing user data in local storage as string
+
+    // Multiple user data storage in local storage
+    let arr = JSON.parse(localStorage.getItem("users")) || [];
+    arr.push(user);
+    localStorage.setItem("users", JSON.stringify(arr));
 
 })
+
+// let dataFromLocalStorage = localStorage.getItem("userData");
+
+// // console.log(dataFromLocalStorage); // Inaccessible data because it is stored as string in local storage, we need to parse it to access the data
+
+// console.log(JSON.parse(dataFromLocalStorage));
+// console.log(JSON.parse(dataFromLocalStorage).fullName); // Accessing fullName from local storage data
+
 
 
