6b4089615407a2620d1ab2d0a75c8d75c68cf2d3
NayandG07 <nayandg8@gmail.com>
2026-07-29 19:19:13 +0530

    Day 18 End

--- Day18/backend/config/db.js
+++ Day18/backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const dns = require('dns');
-// dns.setServers(['8.8.8.8', '8.8.4.4']);
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connection = mongoose.connect(process.env.MONGO_URI);
 
