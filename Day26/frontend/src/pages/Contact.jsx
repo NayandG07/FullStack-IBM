6a8ff6f5c905e39e939983205cb4c4b3373f3d7a
NayandG07 <nayandg8@gmail.com>
2026-08-10 19:01:49 +0530

    Updated

--- Day26/frontend/src/pages/Contact.jsx
+++ Day26/frontend/src/pages/Contact.jsx
@@ -6,10 +6,13 @@ const Contact = () => {
             <form>
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name" name="name" />
+                <br />
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" />
+                <br />
                 <label htmlFor="message">Message:</label>
                 <textarea id="message" name="message"></textarea>
+                <br />
                 <button type="submit">Submit</button>
             </form>
         </div>
