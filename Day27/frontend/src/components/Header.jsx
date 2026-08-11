ebd6faf824f7bf05d4e417fac3b4147b52e571b0
NayandG07 <nayandg8@gmail.com>
2026-08-11 18:19:10 +0530

    Updated

--- Day27/frontend/src/components/Header.jsx
+++ Day27/frontend/src/components/Header.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return (
         <div>
             <h1>My App</h1>
-            <nav style={{ backgroundColor: '#f2f2f2', padding: '10px' }}>
+            <nav style={{ backgroundColor: 'rgb(36, 31, 77)', padding: '10px' }}>
                 <ul style={{ listStyleType: 'none', display: 'flex', justifyContent: 'space-around' }}>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </div>
